ef491500c53cb3daf1accce970b8ae3af817b1b1
PikminGuts92 <PikminGuts92@gmail.com>
2021-12-05 01:37:58 -0500

    Add day 5 part 2 (super hacky ugh)

--- src/day05/mod.rs
+++ src/day05/mod.rs
@@ -30,8 +30,10 @@ pub fn is_horizontal(p1: &(u32, u32), p2: &(u32, u32)) -> bool {
     y1 == y2
 }
 
-pub fn create_line_(p1: &(u32, u32), p2: &(u32, u32)) -> Vec<(u32, u32)> {
-    Vec::new()
+pub fn is_diagonal_45((x1, y1): &(u32, u32), (x2, y2): &(u32, u32)) -> bool {
+    let slope = (*y2 as i32 - *y1 as i32) / (*x2 as i32 - *x1 as i32);
+
+    slope.abs() == 1
 }
 
 pub fn create_line_vertical((x1, y1): &(u32, u32), (x2, y2): &(u32, u32)) -> Vec<(u32, u32)> {
@@ -58,7 +60,30 @@ pub fn create_line_horizontal((x1, y1): &(u32, u32), (x2, y2): &(u32, u32)) -> V
     points
 }
 
-pub fn calculate_intersections(points: &[[[u32; 2]; 2]]) -> u32 {
+pub fn create_line_diagonal_45((x1, y1): &(u32, u32), (x2, y2): &(u32, u32)) -> Vec<(u32, u32)> {
+    let slope = (*y2 as i32 - *y1 as i32) / (*x2 as i32 - *x1 as i32);
+    let ((start_x, start_y), (end_x, end_y)) = if x1 < x2 { ((x1, y1), (x2, y2)) } else { ((x2, y2), (x1, y1)) };
+
+    let mut points = Vec::new();
+    let mut y = *start_y;
+
+    for x in *start_x..(*end_x + 1) {
+        points.push((x, y));
+
+        if y == 0 && slope == -1 {
+            break;
+        }
+
+        y = match slope {
+            1 => y + 1,
+            _ => y - 1
+        };
+    }
+
+    points
+}
+
+pub fn calculate_intersections(points: &[[[u32; 2]; 2]], diagonal: bool) -> u32 {
     let points = points
         .iter()
         .map(|p| (p[0].into(), p[1].into()))
@@ -70,18 +95,20 @@ pub fn calculate_intersections(points: &[[[u32; 2]; 2]]) -> u32 {
         let p1 = (p1.x, p1.y);
         let p2 = (p2.x, p2.y);
 
-        let points;
+        let line_points;
 
         if is_horizontal(&p1, &p2) {
-            points = create_line_horizontal(&p1, &p2);
+            line_points = create_line_horizontal(&p1, &p2);
         } else if is_vertical(&p1, &p2) {
-            points = create_line_vertical(&p1, &p2);
+            line_points = create_line_vertical(&p1, &p2);
+        } else if diagonal && is_diagonal_45(&p1, &p2) {
+            line_points = create_line_diagonal_45(&p1, &p2);
         } else {
             // Skip for now
             continue;
         }
 
-        for p in points {
+        for p in line_points {
             if let Some(count) = placements.get_mut(&p) {
                 *count += 1;
             } else {
@@ -103,10 +130,12 @@ mod tests {
     use super::{*, data::*};
 
     #[rstest]
-    #[case(TEST_DATA_1, 5)]
-    #[case(TEST_DATA_2, 0)]
-    fn calculate_intersections_test<T: AsRef<[[[u32; 2]; 2]]>>(#[case] points: T, #[case] expected: u32) {
-        let result = calculate_intersections(points.as_ref());
+    #[case(TEST_DATA_1, false, 5)]
+    #[case(TEST_DATA_2, false, 6548)]
+    #[case(TEST_DATA_1, true, 12)]
+    #[case(TEST_DATA_2, true, 19663)]
+    fn calculate_intersections_test<T: AsRef<[[[u32; 2]; 2]]>>(#[case] points: T, #[case] diagonal: bool, #[case] expected: u32) {
+        let result = calculate_intersections(points.as_ref(), diagonal);
         assert_eq!(expected, result);
     }
 }
