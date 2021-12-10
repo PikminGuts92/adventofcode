e9b2483bb94bb11b43cbf2456c9db7ddfdc990e1
PikminGuts92 <PikminGuts92@gmail.com>
2021-12-10 01:44:40 -0500

    Add day 10 part 1

---
+++ src/day10/mod.rs
@@ -0,0 +1,142 @@
+#[cfg(test)] mod data;
+
+use std::collections::HashMap;
+
+pub fn find_invalid_chars(chars: &Vec<char>, i: usize, open_stack: &mut Vec<char>, invalid: &mut Vec<char>) -> bool {
+    if i >= chars.len() {
+        return !open_stack.is_empty();
+    }
+
+    /*if open.is_none() {
+        let curr = chars[i];
+
+        let is_open = match curr {
+            '(' | '[' | '{' | '<' => true,
+            _ => false,
+        };
+
+        if is_open {
+            return find_invalid_chars(chars, i + 1, Some(curr), invalid);
+        } else {
+            invalid.push(curr);
+            return false;
+        }
+    }*/
+
+    let open = open_stack.pop();
+    let curr = chars[i];
+
+    // Check if valid
+    let is_valid = match (open, curr) {
+        (Some('('), ')') => true,
+        (Some('['), ']') => true,
+        (Some('{'), '}') => true,
+        (Some('<'), '>') => true,
+        (_, '(') => true,
+        (_, '[') => true,
+        (_, '{') => true,
+        (_, '<') => true,
+        _ => false,
+    };
+
+    if !is_valid {
+        // Fast exit for now
+        invalid.push(curr);
+        return true;
+    }
+
+    let is_closed = match (open, curr) {
+        (Some('('), ')') => true,
+        (Some('['), ']') => true,
+        (Some('{'), '}') => true,
+        (Some('<'), '>') => true,
+        _ => false,
+    };
+
+    if !is_closed {
+        match open {
+            Some('(' | '[' | '{' | '<') => open_stack.push(open.unwrap()),
+            _ => {}
+        }
+
+        match curr {
+            '(' | '[' | '{' | '<' => open_stack.push(curr),
+            _ => {}
+        }
+    }
+
+    /*let new_open = match (is_closed, curr) {
+        (true, _) => None,
+        (_, '(' | '[' | '{' | '<') => Some(curr),
+        _ => open,
+    };*/
+
+    find_invalid_chars(chars, i + 1, open_stack, invalid)
+}
+
+pub fn find_syntax_errors_in_line(line: &str) -> i64 {
+    if line.is_empty() {
+        return 0;
+    }
+
+    let chars = line.chars().collect::<Vec<_>>();
+
+    let mut open_stack = Vec::new();
+    let mut invalid_chars = Vec::new();
+    let complete = find_invalid_chars(&chars, 0, &mut open_stack, &mut invalid_chars);
+
+    println!("Invalid: {:?}", invalid_chars);
+
+    if !complete {
+        println!("Not VALID");
+        return 0;
+    }
+
+    // Calculate score
+    let score = invalid_chars
+        .iter()
+        .map(|c| match c {
+            ')' => 3,
+            ']' => 57,
+            '}' => 1197,
+            '>' => 25137,
+            _ => 0,
+        })
+        .sum();
+
+    println!("Score: {}\n", score);
+
+    score
+}
+
+pub fn find_syntax_errors(lines: &[&str]) -> i64 {
+    lines
+        .iter()
+        .enumerate()
+        .map(|(i, l)| {
+            println!("{} - {}", i, l);
+            find_syntax_errors_in_line(l)
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::*;
+    use super::{*, data::*};
+
+    #[rstest]
+    #[case(TEST_DATA_1, 0, 0)]
+    pub fn find_syntax_errors_in_line_test<T: AsRef<[&'static str]>>(#[case] lines: T, #[case] i: usize, #[case] expected: i64) {
+        let result = find_syntax_errors_in_line(lines.as_ref()[i]);
+        assert_eq!(expected, result);
+    }
+
+    #[rstest]
+    #[case(TEST_DATA_1, 26397)]
+    #[case(TEST_DATA_2, 366027)]
+    pub fn find_syntax_errors_test<T: AsRef<[&'static str]>>(#[case] lines: T, #[case] expected: i64) {
+        let result = find_syntax_errors(lines.as_ref());
+        assert_eq!(expected, result);
+    }
+}
