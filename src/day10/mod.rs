d418d3cd204a2e3dc23a0e638fac4cb75f1a08a6
PikminGuts92 <PikminGuts92@gmail.com>
2021-12-10 02:18:54 -0500

    Add day 10 part 2

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -4,24 +4,8 @@ use std::collections::HashMap;
 
 pub fn find_invalid_chars(chars: &Vec<char>, i: usize, open_stack: &mut Vec<char>, invalid: &mut Vec<char>) -> bool {
     if i >= chars.len() {
-        return !open_stack.is_empty();
+        return open_stack.is_empty();
     }
-
-    /*if open.is_none() {
-        let curr = chars[i];
-
-        let is_open = match curr {
-            '(' | '[' | '{' | '<' => true,
-            _ => false,
-        };
-
-        if is_open {
-            return find_invalid_chars(chars, i + 1, Some(curr), invalid);
-        } else {
-            invalid.push(curr);
-            return false;
-        }
-    }*/
 
     let open = open_stack.pop();
     let curr = chars[i];
@@ -42,7 +26,7 @@ pub fn find_invalid_chars(chars: &Vec<char>, i: usize, open_stack: &mut Vec<char
     if !is_valid {
         // Fast exit for now
         invalid.push(curr);
-        return true;
+        return false;
     }
 
     let is_closed = match (open, curr) {
@@ -65,12 +49,6 @@ pub fn find_invalid_chars(chars: &Vec<char>, i: usize, open_stack: &mut Vec<char
         }
     }
 
-    /*let new_open = match (is_closed, curr) {
-        (true, _) => None,
-        (_, '(' | '[' | '{' | '<') => Some(curr),
-        _ => open,
-    };*/
-
     find_invalid_chars(chars, i + 1, open_stack, invalid)
 }
 
@@ -85,10 +63,7 @@ pub fn find_syntax_errors_in_line(line: &str) -> i64 {
     let mut invalid_chars = Vec::new();
     let complete = find_invalid_chars(&chars, 0, &mut open_stack, &mut invalid_chars);
 
-    println!("Invalid: {:?}", invalid_chars);
-
-    if !complete {
-        println!("Not VALID");
+    if invalid_chars.is_empty() || complete {
         return 0;
     }
 
@@ -104,20 +79,57 @@ pub fn find_syntax_errors_in_line(line: &str) -> i64 {
         })
         .sum();
 
-    println!("Score: {}\n", score);
-
     score
 }
 
 pub fn find_syntax_errors(lines: &[&str]) -> i64 {
     lines
         .iter()
-        .enumerate()
-        .map(|(i, l)| {
-            println!("{} - {}", i, l);
-            find_syntax_errors_in_line(l)
-        })
+        .map(|l| find_syntax_errors_in_line(l))
         .sum()
+}
+
+pub fn find_incomplete_in_line(line: &str) -> i64 {
+    if line.is_empty() {
+        return 0;
+    }
+
+    let chars = line.chars().collect::<Vec<_>>();
+
+    let mut open_stack = Vec::new();
+    let mut invalid_chars = Vec::new();
+    let complete = find_invalid_chars(&chars, 0, &mut open_stack, &mut invalid_chars);
+
+    if !invalid_chars.is_empty() || complete {
+        return 0;
+    }
+
+    // Calculate score
+    let score = open_stack
+        .iter()
+        .rev()
+        .map(|c| match c {
+            '(' => 1,
+            '[' => 2,
+            '{' => 3,
+            '<' => 4,
+            _ => panic!(),
+        })
+        .fold(0, |acc, s| acc * 5 + s);
+
+    score
+}
+
+pub fn find_incomplete(lines: &[&str]) -> i64 {
+    let mut scores = lines
+        .iter()
+        .map(|l| find_incomplete_in_line(l))
+        .filter(|s| s > &0)
+        .collect::<Vec<_>>();
+
+    // Assume always odd scores...
+    scores.sort();
+    scores[(scores.len() / 2)]
 }
 
 #[cfg(test)]
@@ -137,6 +149,21 @@ mod tests {
     #[case(TEST_DATA_2, 366027)]
     pub fn find_syntax_errors_test<T: AsRef<[&'static str]>>(#[case] lines: T, #[case] expected: i64) {
         let result = find_syntax_errors(lines.as_ref());
+        assert_eq!(expected, result);
+    }
+
+    #[rstest]
+    #[case(TEST_DATA_1, 0, 288957)]
+    pub fn find_incomplete_in_line_test<T: AsRef<[&'static str]>>(#[case] lines: T, #[case] i: usize, #[case] expected: i64) {
+        let result = find_incomplete_in_line(lines.as_ref()[i]);
+        assert_eq!(expected, result);
+    }
+
+    #[rstest]
+    #[case(TEST_DATA_1, 288957)]
+    #[case(TEST_DATA_2, 1118645287)]
+    pub fn find_incomplete_test<T: AsRef<[&'static str]>>(#[case] lines: T, #[case] expected: i64) {
+        let result = find_incomplete(lines.as_ref());
         assert_eq!(expected, result);
     }
 }
