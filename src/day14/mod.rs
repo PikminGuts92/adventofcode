09d35407fc416ef3cd256a5a1de29be7703ee829
PikminGuts92 <PikminGuts92@gmail.com>
2021-12-17 22:02:58 -0500

    Refactor and clean up day 14

--- src/day14/mod.rs
+++ src/day14/mod.rs
@@ -6,43 +6,16 @@ const MAX_CHARS: usize = 22; // 'A' -> 'V'
 
 pub fn grow_polymer(template: &str, rules: &[([char; 2], char)], mut steps: u32) -> (i64, i64) {
     // Transform rules
-    let mut transforms = HashMap::new();
-
-    let rules_hashed = rules
+    let rules = rules
         .iter()
-        .map(|([a, b], _)| (*a, *b))
-        .collect::<HashSet<_>>();
+        .map(|([a, b], c)| {
+            let a = (*a as u8) - ('A' as u8);
+            let b = (*b as u8) - ('A' as u8);
+            let c = (*c as u8) - ('A' as u8);
 
-    for (seq, ins) in rules.iter() {
-        let a = seq[0];
-        let b = seq[1];
-        let c = *ins;
-
-        let ac = (a, c);
-        let cb = (c, b);
-
-        let b1 = rules_hashed
-            .contains(&ac)
-            .then_some(ac);
-
-        let b2 = rules_hashed
-            .contains(&cb)
-            .then_some(cb);
-
-        if b1.is_some() || b2.is_some() {
-            transforms.insert((a, b), (b1, b2));
-
-            /*let b1_value = b1
-                .map(|(a, b)| format!("{a}{b}"))
-                .unwrap_or(String::from("??"));
-
-            let b2_value = b2
-                .map(|(a, b)| format!("{a}{b}"))
-                .unwrap_or(String::from("??"));
-
-            println!("({}, {})", b1_value, b2_value);*/
-        }
-    }
+            ((a, b), [(a, c), (c, b)])
+        })
+        .collect::<HashMap<_, _>>();
 
     let mut char_counts = [0i64; MAX_CHARS];
     let mut seq_counts = [[0i64; MAX_CHARS]; MAX_CHARS];
@@ -50,20 +23,20 @@ pub fn grow_polymer(template: &str, rules: &[([char; 2], char)], mut steps: u32)
     // Setup initial state
     {
         let mut chars_iter = template.chars();
-        let mut a_char = chars_iter.next();
+        let mut prev_char = chars_iter.next();
 
-        while let Some(b) = chars_iter.next() {
-            let a = a_char.unwrap();
+        while let Some(b_char) = chars_iter.next() {
+            let a_char = prev_char.unwrap();
 
-            if rules_hashed.contains(&(a, b)) {
-                let a = ((a as u8) - ('A' as u8)) as usize;
-                let b = ((b as u8) - ('A' as u8)) as usize;
+            let a = (a_char as u8) - ('A' as u8);
+            let b = (b_char as u8) - ('A' as u8);
 
-                seq_counts[a][b] += 1;
+            if rules.contains_key(&(a, b)) {
+                seq_counts[a as usize][b as usize] += 1;
             }
 
             // Update
-            a_char = Some(b);
+            prev_char = Some(b_char);
         }
 
         // Setup initial char counts
@@ -71,90 +44,44 @@ pub fn grow_polymer(template: &str, rules: &[([char; 2], char)], mut steps: u32)
             let c = ((c as u8) - ('A' as u8)) as usize;
             char_counts[c] += 1;
         }
-
-        /*for j in 1..template.len() {
-            let i = j - 1;
-
-            let a = template.chars().nth(i).unwrap();
-            let b = template.chars().nth(j).unwrap();
-
-            if rules_hashed.contains(&(a, b)) {
-                let a = ((a as u8) - ('A' as u8)) as usize;
-                let b = ((b as u8) - ('A' as u8)) as usize;
-
-                seq_counts[a][b] += 1;
-            }
-        }*/
     }
 
-    println!("Step #0");
-    let letter_count: i64 = char_counts.iter().sum();
-    println!("{letter_count} letters");
-
-    let seq_count: i64 = seq_counts.iter().flatten().sum();
-    println!("{seq_count} sequences\n");
-
     // Iterate steps
-    let mut i = 1;
     while steps > 0 {
         let mut updates = Vec::new();
 
-        for a in 0..seq_counts.len() {
-            for b in 0..seq_counts[a].len() {
-                let a_char = ((a as u8) + ('A' as u8)) as char;
-                let b_char = ((b as u8) + ('A' as u8)) as char;
-
-                let count = seq_counts[a][b];
-                if count <= 0 {
-                    continue;
-                }
-
-                if let Some((b1, b2)) = transforms.get(&(a_char, b_char)) {
-                    let c = ((b1.unwrap().1 as u8) - 'A' as u8) as usize;
-                    char_counts[c] += count;
-
-                    // Remove
-                    updates.push(((a_char, b_char), -count));
-
-                    // Add
-                    if let Some(b) = b1 {
-                        updates.push((*b, count));
-                    }
-
-                    // Add
-                    if let Some(b) = b2 {
-                        updates.push((*b, count));
-                    }
-                } else {
-                    println!("No match for {}{}", a_char, b_char);
-                }
+        for ((sa, sb), [(a, c), (_, b)]) in rules.iter() {
+            let seq_count = seq_counts[*sa as usize][*sb as usize];
+            if seq_count <= 0 {
+                continue;
             }
+
+            char_counts[*c as usize] += seq_count;
+
+            updates.push(((*a, *b), -seq_count));
+            updates.push(((*a, *c), seq_count));
+            updates.push(((*c, *b), seq_count));
         }
 
         // Update counts
-        println!("Step #{i}");
-        println!("Applying {} updates", updates.len());
-
         for ((a, b), count) in updates {
-            let a = ((a as u8) - ('A' as u8)) as usize;
-            let b = ((b as u8) - ('A' as u8)) as usize;
-
-            seq_counts[a][b] += count;
+            seq_counts[a as usize][b as usize] += count;
         }
 
-        let letter_count: i64 = char_counts.iter().sum();
-        println!("{letter_count} letters");
-
-        let seq_count: i64 = seq_counts.iter().flatten().sum();
-        println!("{seq_count} sequences\n");
-
         steps -= 1;
-        i += 1;
     }
 
-    let min = char_counts.iter().filter(|n| n.is_positive()).min().map(|n| *n).unwrap_or_default();
-    let max = char_counts.iter().max().map(|n| *n).unwrap_or_default();
-
+    // Find min/max character
+    let (min, max) = char_counts
+        .iter()
+        .fold((i64::MAX, i64::MIN), |(min, max), v: &i64 | {
+            if v > &0 {
+                (min.min(*v), max.max(*v))
+            } else {
+                (min, max)
+            }
+        });
+    
     (min, max)
 }
 
