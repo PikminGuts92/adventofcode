6e1151c1c68bf53203f6a4f6e5ef7184721bb5e3
PikminGuts92 <PikminGuts92@gmail.com>
2021-12-27 11:42:46 -0500

    Convert day 22 data to tuple slices

--- src/day22/mod.rs
+++ src/day22/mod.rs
@@ -1,13 +1,22 @@
 #[cfg(test)] mod data;
 
+use std::ops::RangeInclusive;
+
+pub fn find_on_cube_count(data: &[(bool, [RangeInclusive<i64>; 3])]) -> i64 {
+    0
+}
+
 #[cfg(test)]
 mod tests {
     use rstest::*;
     use super::{*, data::*};
 
     #[rstest]
-    //#[case]
-    pub fn test() {
-        assert_eq!(true, true);
+    #[case(TEST_DATA_0, 39)]
+    #[case(TEST_DATA_1, 590784)]
+    #[case(TEST_DATA_2, 39)]
+    pub fn find_on_cube_count_test<T: AsRef<[(bool, [RangeInclusive<i64>; 3])]>>(#[case] data: T, #[case] expected: i64) {
+        let result = find_on_cube_count(data.as_ref());
+        assert_eq!(expected, result);
     }
 }
