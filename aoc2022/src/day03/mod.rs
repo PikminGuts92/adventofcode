10302cfd45cb153fb34fe58a34da75a90e90ebf9
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2022-12-03 13:18:08 -0500

    Add part 2 for day 3

--- aoc2022/src/day03/mod.rs
+++ aoc2022/src/day03/mod.rs
@@ -1,49 +1,38 @@
 #[cfg(test)] mod data;
 
 pub struct Compartment {
-    pub part1: [u8; 52],
-    pub part2: [u8; 52],
-    pub shared: Vec<char>,
-    pub shared_points: u32,
+    pub points: u32,
 }
 
 impl Compartment {
     fn new() -> Compartment {
         Compartment {
-            part1: [0u8; 52],
-            part2: [0u8; 52],
-            shared: Vec::new(),
-            shared_points: 0u32,
+            points: 0u32,
         }
     }
 
-    pub fn from_str(data: &str) -> Compartment {
+    pub fn from_str_group<const N: usize>(data: &[&str; N]) -> Compartment {
+        let mut char_counts = [[0u8; 52]; N];
         let mut comp = Compartment::new();
 
-        // Split data in half
-        let (d1, d2) = data.split_at(data.len() / 2);
-
         // Enumerate data in compartments
-        for (data, part) in [(d1, &mut comp.part1), (d2, &mut comp.part2)] {
+        for (data, part) in data.iter().zip(&mut char_counts) {
             for c in data.chars() {
                 let index = get_index(c);
                 part[index] += 1;
             }
         }
 
-        // Find chars in both
-        for i in 0..comp.part1.len() {
-            let (c1, c2) = (comp.part1[i], comp.part2[i]);
-
-            if c1 == 0 && c2 == 0 {
+        // Find chars shared between data
+        for i in 0..52 {
+            let all_zero = char_counts.iter().all(|c| c[i].eq(&0));
+            if all_zero {
                 continue;
             }
 
-            // TODO: Add shared chars?
-
-            let in_both = c1.ge(&1) && c2.ge(&1);
-            if in_both {
-                comp.shared_points += (i + 1) as u32;
+            let in_all = char_counts.iter().all(|c| c[i].ge(&1));
+            if in_all {
+                comp.points += (i + 1) as u32;
             }
         }
 
@@ -59,17 +48,31 @@ pub fn get_index(c: char) -> usize {
     }
 }
 
-pub fn parse_compartments(data: &[&str]) -> Vec<Compartment> {
+pub fn parse_compartments_in_halves(data: &[&str]) -> Vec<Compartment> {
     data
         .iter()
-        .map(|d| Compartment::from_str(d))
+        .map(|d| {
+            // Split data in half
+            let (d1, d2) = d.split_at(d.len() / 2);
+
+            Compartment::from_str_group(&[d1, d2])
+        })
+        .collect()
+}
+
+pub fn parse_compartments_in_groups_of_3(data: &[&str]) -> Vec<Compartment> {
+    data
+        .chunks(3)
+        .map(|d| {
+            Compartment::from_str_group(&[d[0], d[1], d[2]])
+        })
         .collect()
 }
 
 pub fn find_sum_of_priorities(compartments: &[Compartment]) -> u32 {
     compartments
         .iter()
-        .map(|comp| comp.shared_points)
+        .map(|comp| comp.points)
         .sum()
 }
 
@@ -98,9 +101,19 @@ mod tests {
 
     #[rstest]
     #[case(TEST_DATA_0, 157)]
-    #[case(TEST_DATA_1, 0)]
+    #[case(TEST_DATA_1, 7746)]
     fn find_sum_of_priorities_test<const N: usize>(#[case] raw_data: [&str; N], #[case] expected: u32) {
-        let compartments = parse_compartments(&raw_data);
+        let compartments = parse_compartments_in_halves(&raw_data);
+        let result = find_sum_of_priorities(&compartments);
+
+        assert_eq!(expected, result);
+    }
+
+    #[rstest]
+    #[case(TEST_DATA_0, 70)]
+    #[case(TEST_DATA_1, 2604)]
+    fn find_sum_of_priorities_group_test<const N: usize>(#[case] raw_data: [&str; N], #[case] expected: u32) {
+        let compartments = parse_compartments_in_groups_of_3(&raw_data);
         let result = find_sum_of_priorities(&compartments);
 
         assert_eq!(expected, result);
