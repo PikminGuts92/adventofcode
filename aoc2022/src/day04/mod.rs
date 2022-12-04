9529ac4a48ea4e632ccc2ca79a3f78217573d059
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2022-12-04 06:00:26 -0500

    Add part 2 day 4

--- aoc2022/src/day04/mod.rs
+++ aoc2022/src/day04/mod.rs
@@ -12,10 +12,36 @@ pub fn is_one_fully_contained(elf_1: &(u32, u32), elf_2: &(u32, u32)) -> bool {
     false
 }
 
+pub fn has_overlap(elf_1: &(u32, u32), elf_2: &(u32, u32)) -> bool {
+    let ranges = [(elf_1, elf_2), (elf_2, elf_1)];
+
+    for ((r1_min, r1_max), (r2_min, r2_max)) in ranges {
+        if r1_max.lt(r2_min) {
+            continue;
+        }
+
+        let min = *r1_min.max(r2_min);
+        let max = *r1_max.min(r2_max);
+
+        if max.ge(&min) {
+            return true;
+        }
+    }
+
+    false
+}
+
 pub fn calc_fully_contained_counts(pairs: &[[(u32, u32); 2]]) -> usize {
     pairs
         .iter()
         .filter(|[a, b]| is_one_fully_contained(a, b))
+        .count()
+}
+
+pub fn calc_overlap_counts(pairs: &[[(u32, u32); 2]]) -> usize {
+    pairs
+        .iter()
+        .filter(|[a, b]| has_overlap(a, b))
         .count()
 }
 
@@ -29,6 +55,15 @@ mod tests {
     #[case(TEST_DATA_1, 475)]
     fn calc_fully_contained_counts_test<const N: usize>(#[case] data: [[(u32, u32); 2]; N], #[case] expected: usize) {
         let result = calc_fully_contained_counts(&data);
+
+        assert_eq!(expected, result);
+    }
+
+    #[rstest]
+    #[case(TEST_DATA_0, 4)]
+    #[case(TEST_DATA_1, 825)]
+    fn calc_overlap_counts_test<const N: usize>(#[case] data: [[(u32, u32); 2]; N], #[case] expected: usize) {
+        let result = calc_overlap_counts(&data);
 
         assert_eq!(expected, result);
     }
