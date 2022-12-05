b9f03510257c8af2d20ff5293129417ae4f6b7a8
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2022-12-05 08:16:34 -0500

    Implement part 1 for day 5

--- aoc2022/src/day05/mod.rs
+++ aoc2022/src/day05/mod.rs
@@ -1,11 +1,49 @@
 #[cfg(test)] mod data;
 
 pub fn transform_data_stacks<const N: usize>(raw_stacks: &[[Option<char>; N]]) -> Vec<Vec<char>> {
-    todo!()
+    let stack_count = N;
+    let mut stacks = vec![Vec::new(); stack_count];
+
+    if raw_stacks.is_empty() {
+        return stacks;
+    }
+
+    for j in 0..stack_count {
+        let stack = &mut stacks[j];
+        let mut i = raw_stacks.len() - 1;
+
+        while let Some(Some(item)) = raw_stacks[i].get(j) {
+            stack.push(*item);
+
+            if i.lt(&1) {
+                break;
+            }
+
+            i -= 1;
+        }
+    }
+
+    stacks
 }
 
-pub fn find_top_stacks_in_rearrangement(stacks: &[Vec<char>], instructions: &[(u32, u32, u32)]) -> String {
-    todo!()
+pub fn get_top_crates(stacks: &[Vec<char>]) -> String {
+    String::from_iter(stacks
+        .iter()
+        .map(|s| s.last().unwrap_or(&'\0')))
+}
+
+pub fn find_top_stacks_in_rearrangement(stacks: &mut [Vec<char>], instructions: &[(u32, u32, u32)]) -> String {
+    for (count, from_stack, to_stack) in instructions {
+        let from_idx = (from_stack - 1) as usize;
+        let to_idx = (to_stack - 1) as usize;
+
+        for _ in 0..*count {
+            let item = stacks[from_idx].pop().unwrap();
+            stacks[to_idx].push(item);
+        }
+    }
+
+    get_top_crates(stacks)
 }
 
 #[cfg(test)]
@@ -15,13 +53,13 @@ mod tests {
 
     #[rstest]
     #[case(TEST_DATA_STACKS_0, TEST_DATA_INSTRUCTIONS_0, "CMZ")]
-    #[case(TEST_DATA_STACKS_1, TEST_DATA_INSTRUCTIONS_1, "")]
+    #[case(TEST_DATA_STACKS_1, TEST_DATA_INSTRUCTIONS_1, "LBLVVTVLP")]
     fn find_top_stacks_in_rearrangement_test<const N1: usize, const N2: usize, const M: usize>(
         #[case] raw_data: [[Option<char>; N1]; N2],
         #[case] instructions: [(u32, u32, u32); M],
         #[case] expected: &str) {
-        let stacks = transform_data_stacks(&raw_data);
-        let result = find_top_stacks_in_rearrangement(&stacks, &instructions);
+        let mut stacks = transform_data_stacks(&raw_data);
+        let result = find_top_stacks_in_rearrangement(&mut stacks, &instructions);
 
         assert_eq!(expected, &result);
     }
