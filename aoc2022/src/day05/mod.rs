a66494093ead96aedf48166dd96138025a44ddbd
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2022-12-05 08:26:10 -0500

    Add part 2 for day 5

--- aoc2022/src/day05/mod.rs
+++ aoc2022/src/day05/mod.rs
@@ -46,6 +46,26 @@ pub fn find_top_stacks_in_rearrangement(stacks: &mut [Vec<char>], instructions: 
     get_top_crates(stacks)
 }
 
+pub fn find_top_stacks_in_rearrangement_retain_order(stacks: &mut [Vec<char>], instructions: &[(u32, u32, u32)]) -> String {
+    for (count, from_stack, to_stack) in instructions {
+        let from_idx = (from_stack - 1) as usize;
+        let to_idx = (to_stack - 1) as usize;
+
+        let mut items = Vec::new();
+
+        for _ in 0..*count {
+            let item = stacks[from_idx].pop().unwrap();
+            items.push(item);
+        }
+
+        while let Some(item) = items.pop() {
+            stacks[to_idx].push(item);
+        }
+    }
+
+    get_top_crates(stacks)
+}
+
 #[cfg(test)]
 mod tests {
     use rstest::*;
@@ -60,6 +80,19 @@ mod tests {
         #[case] expected: &str) {
         let mut stacks = transform_data_stacks(&raw_data);
         let result = find_top_stacks_in_rearrangement(&mut stacks, &instructions);
+
+        assert_eq!(expected, &result);
+    }
+
+    #[rstest]
+    #[case(TEST_DATA_STACKS_0, TEST_DATA_INSTRUCTIONS_0, "MCD")]
+    #[case(TEST_DATA_STACKS_1, TEST_DATA_INSTRUCTIONS_1, "TPFFBDRJD")]
+    fn find_top_stacks_in_rearrangement_retain_order_test<const N1: usize, const N2: usize, const M: usize>(
+        #[case] raw_data: [[Option<char>; N1]; N2],
+        #[case] instructions: [(u32, u32, u32); M],
+        #[case] expected: &str) {
+        let mut stacks = transform_data_stacks(&raw_data);
+        let result = find_top_stacks_in_rearrangement_retain_order(&mut stacks, &instructions);
 
         assert_eq!(expected, &result);
     }
