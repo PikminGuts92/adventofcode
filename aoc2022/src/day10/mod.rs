7046f593d8a2e97dddf6b788937690dc6365e265
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2022-12-10 10:16:30 -0500

    Refactor into iterator struct

--- aoc2022/src/day10/mod.rs
+++ aoc2022/src/day10/mod.rs
@@ -5,6 +5,82 @@ pub enum Instruction {
     AddX(i32)
 }
 
+pub struct Program<'a> {
+    cycle: usize,
+    register_x: i32,
+    instructions: &'a [Instruction],
+    instruction_idx: usize,
+    current_instruction: Option<(i32, i32)>,
+}
+
+impl<'a> Program<'a> {
+    pub fn from_instructions(instructions: &'a [Instruction]) -> Program {
+        Program {
+            cycle: 1usize,
+            register_x: 1i32,
+            instructions,
+            instruction_idx: 0usize,
+            current_instruction: None,
+        }
+    }
+
+    fn run_cycle(&mut self) {
+        if self.has_ended() {
+            return;
+        }
+
+        if self.current_instruction.is_none() {
+            self.load_instruction();
+        }
+
+        match self.current_instruction {
+            Some((1, add)) => {
+                self.register_x += add;
+                self.current_instruction = None;
+            },
+            Some((c, add)) => {
+                self.current_instruction = Some((c - 1, add));
+            },
+            _ => {
+                // Do nothing
+            }
+        };
+
+        self.cycle += 1;
+    }
+
+    fn load_instruction(&mut self) {
+        let Some(instruction) = self.instructions.get(self.instruction_idx) else {
+            return;
+        };
+
+        self.instruction_idx += 1;
+
+        self.current_instruction = match instruction {
+            Instruction::NOP => Some((1, 0i32)),
+            Instruction::AddX(add) => Some((2, *add))
+        };
+    }
+
+    fn has_ended(&self) -> bool {
+        self.instruction_idx.ge(&self.instructions.len())
+            && self.current_instruction.is_none()
+    }
+}
+
+impl<'a> Iterator for Program<'a> {
+    type Item = (usize, i32);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.has_ended() {
+            return None;
+        }
+
+        self.run_cycle();
+        Some((self.cycle, self.register_x))
+    }
+}
+
 pub fn execute_instructions(instructions: &[Instruction], cycles: &[usize]) -> i32 {
     let mut cycle = 1usize;
     let mut register_x = 1i32;
@@ -38,6 +114,27 @@ pub fn execute_instructions(instructions: &[Instruction], cycles: &[usize]) -> i
     cycle_sum
 }
 
+pub fn execute_instructions_2(instructions: &[Instruction], cycles: &[usize]) -> i32 {
+    let mut cycle_index = 0usize;
+    let mut cycle_sum = 0i32;
+
+    let program = Program::from_instructions(instructions);
+
+    // Run instructions
+    for (cycle, reg_x) in program {
+        // Update tracked cycles
+        if cycles
+            .get(cycle_index)
+            .map(|c| c.eq(&cycle))
+            .unwrap_or_default() {
+            cycle_sum += (cycle as i32) * reg_x;
+            cycle_index += 1;
+        }
+    }
+
+    cycle_sum
+}
+
 #[cfg(test)]
 mod tests {
     use rstest::*;
@@ -48,7 +145,7 @@ mod tests {
     #[case(TEST_DATA_1, 14820)]
     fn execute_instructions_test<const N: usize>(#[case] data: [Instruction; N], #[case] expected: i32) {
         let cycles = [20, 60, 100, 140, 180, 220];
-        let result = execute_instructions(&data, &cycles);
+        let result = execute_instructions_2(&data, &cycles);
 
         assert_eq!(expected, result);
     }
