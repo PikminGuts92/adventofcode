742082df3535eb01ccb083e77e7e3eff8e56d8a8
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2023-12-01 05:50:47 -0500

    Add part 1 day 1

--- aoc2023/src/day01/mod.rs
+++ aoc2023/src/day01/mod.rs
@@ -1,12 +1,72 @@
 #[cfg(test)] mod data;
 
+fn get_digits_from_row_1(row: &str) -> u32 {
+    let left_digit = row
+        .chars()
+        .find(|c| c.is_numeric())
+        .unwrap();
+
+    let right_digit = row
+        .chars()
+        .rev()
+        .find(|c| c.is_numeric())
+        .unwrap();
+
+    let s = String::from_utf8(vec![ left_digit as u8, right_digit as u8 ]).unwrap();
+
+    s.parse::<u32>().unwrap()
+}
+
+fn calculate_sum_from_rows_1(rows: &[&str]) -> u32 {
+    rows
+        .iter()
+        .map(|r| get_digits_from_row_1(r))
+        .sum()
+}
+
+fn get_digits_from_row_2(row: &str) -> u32 {
+    let left_digit = row
+        .chars()
+        .find(|c| c.is_numeric())
+        .unwrap();
+
+    let right_digit = row
+        .chars()
+        .rev()
+        .find(|c| c.is_numeric())
+        .unwrap();
+
+    let s = String::from_utf8(vec![ left_digit as u8, right_digit as u8 ]).unwrap();
+
+    s.parse::<u32>().unwrap()
+}
+
+fn calculate_sum_from_rows_2(rows: &[&str]) -> u32 {
+    rows
+        .iter()
+        .map(|r| get_digits_from_row_2(r))
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use rstest::*;
     use super::{*, data::*};
 
     #[rstest]
-    fn test_0() {
-        assert_eq!(1, 1);
+    #[case(TEST_DATA_0, 142)]
+    #[case(TEST_DATA_1, 54450)]
+    fn calculate_sum_from_rows_1_test<const N: usize>(#[case] data: [&str; N], #[case] expected: u32) {
+        let actual = calculate_sum_from_rows_1(&data);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[rstest]
+    #[case(TEST_DATA_2, 0)]
+    fn calculate_sum_from_rows_2_test<const N: usize>(#[case] data: [&str; N], #[case] expected: u32) {
+        let actual = calculate_sum_from_rows_2(&data);
+
+        assert_eq!(expected, actual);
     }
 }
