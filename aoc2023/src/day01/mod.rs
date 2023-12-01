b091e62e555c7e01120e0e870ee6eaf6c23c19db
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2023-12-01 05:58:43 -0500

    Switch str to u8 slice

--- aoc2023/src/day01/mod.rs
+++ aoc2023/src/day01/mod.rs
@@ -1,47 +1,47 @@
 #[cfg(test)] mod data;
 
-fn get_digits_from_row_1(row: &str) -> u32 {
+fn get_digits_from_row_1(row: &[u8]) -> u32 {
     let left_digit = row
-        .chars()
-        .find(|c| c.is_numeric())
+        .iter()
+        .find(|c| c.is_ascii_digit())
         .unwrap();
 
     let right_digit = row
-        .chars()
+        .iter()
         .rev()
-        .find(|c| c.is_numeric())
+        .find(|c| c.is_ascii_digit())
         .unwrap();
 
-    let s = String::from_utf8(vec![ left_digit as u8, right_digit as u8 ]).unwrap();
+    let s = String::from_utf8(vec![ *left_digit, *right_digit ]).unwrap();
 
     s.parse::<u32>().unwrap()
 }
 
-fn calculate_sum_from_rows_1(rows: &[&str]) -> u32 {
+fn calculate_sum_from_rows_1(rows: &[&[u8]]) -> u32 {
     rows
         .iter()
         .map(|r| get_digits_from_row_1(r))
         .sum()
 }
 
-fn get_digits_from_row_2(row: &str) -> u32 {
+fn get_digits_from_row_2(row: &[u8]) -> u32 {
     let left_digit = row
-        .chars()
-        .find(|c| c.is_numeric())
+        .iter()
+        .find(|c| c.is_ascii_digit())
         .unwrap();
 
     let right_digit = row
-        .chars()
+        .iter()
         .rev()
-        .find(|c| c.is_numeric())
+        .find(|c| c.is_ascii_digit())
         .unwrap();
 
-    let s = String::from_utf8(vec![ left_digit as u8, right_digit as u8 ]).unwrap();
+    let s = String::from_utf8(vec![ *left_digit, *right_digit ]).unwrap();
 
     s.parse::<u32>().unwrap()
 }
 
-fn calculate_sum_from_rows_2(rows: &[&str]) -> u32 {
+fn calculate_sum_from_rows_2(rows: &[&[u8]]) -> u32 {
     rows
         .iter()
         .map(|r| get_digits_from_row_2(r))
@@ -56,7 +56,7 @@ mod tests {
     #[rstest]
     #[case(TEST_DATA_0, 142)]
     #[case(TEST_DATA_1, 54450)]
-    fn calculate_sum_from_rows_1_test<const N: usize>(#[case] data: [&str; N], #[case] expected: u32) {
+    fn calculate_sum_from_rows_1_test<const N: usize>(#[case] data: [&[u8]; N], #[case] expected: u32) {
         let actual = calculate_sum_from_rows_1(&data);
 
         assert_eq!(expected, actual);
@@ -64,7 +64,7 @@ mod tests {
 
     #[rstest]
     #[case(TEST_DATA_2, 0)]
-    fn calculate_sum_from_rows_2_test<const N: usize>(#[case] data: [&str; N], #[case] expected: u32) {
+    fn calculate_sum_from_rows_2_test<const N: usize>(#[case] data: [&[u8]; N], #[case] expected: u32) {
         let actual = calculate_sum_from_rows_2(&data);
 
         assert_eq!(expected, actual);
