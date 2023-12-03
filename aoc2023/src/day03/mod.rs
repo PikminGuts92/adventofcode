6a569cc8392f3b2a03be136f5e596cae83a22fa2
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2023-12-03 08:28:36 -0500

    Add part 2 day 3

--- aoc2023/src/day03/mod.rs
+++ aoc2023/src/day03/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 #[cfg(test)] mod data;
 
 fn find_numbers_in_row(row: &[u8]) -> Vec<(u32, usize, usize)> {
@@ -64,8 +66,6 @@ fn find_sum_of_adjacent_numbers(data: &[&[u8]]) -> u32 {
     for y in 0..data.len() {
         let numbers = find_numbers_in_row(data[y]);
 
-        println!("{numbers:?}"); // TODO: Remove
-
         for (number, start_pos, end_pos) in numbers {
             // Check start pos
             if check_adjacent_for_symbol(y, start_pos, &data) {
@@ -83,6 +83,86 @@ fn find_sum_of_adjacent_numbers(data: &[&[u8]]) -> u32 {
     current_sum
 }
 
+fn find_gears_positions(data: &[&[u8]]) -> Vec<(usize, usize)> {
+    let mut gears = Vec::new();
+
+    for y in 0..data.len() {
+        for x in 0..data[y].len() {
+            let c = data[y][x];
+
+            if c.eq(&b'*') {
+                gears.push((y, x));
+            }
+        }
+    }
+
+    gears
+}
+
+fn check_adjacent_for_numbers(y: usize, x: usize, data_map: &HashMap<(usize, usize), u32>) -> Option<Vec<u32>> {
+    let surroundings = [
+        (Some(y), x.checked_sub(1)),
+        (Some(y), x.checked_add(1)),
+        (y.checked_sub(1), Some(x)),
+        (y.checked_add(1), Some(x)),
+        (y.checked_sub(1), x.checked_sub(1)),
+        (y.checked_add(1), x.checked_add(1)),
+        (y.checked_sub(1), x.checked_add(1)),
+        (y.checked_add(1), x.checked_sub(1)),
+    ];
+
+    let mut found_numbers = HashSet::new();
+
+    for (y_pos, x_pos) in surroundings {
+        let Some(y) = y_pos else {
+            continue;
+        };
+
+        let Some(x) = x_pos else {
+            continue;
+        };
+
+        if let Some(n) = data_map.get(&(y, x)) {
+            found_numbers.insert(*n);
+        }
+    }
+
+    if !found_numbers.is_empty() {
+        Some(found_numbers.into_iter().collect())
+    } else {
+        None
+    }
+}
+
+fn find_sum_of_adjacent_numbers_for_gears(data: &[&[u8]]) -> u32 {
+    let mut current_sum = 0;
+    let mut numbers_map = HashMap::new();
+
+    for y in 0..data.len() {
+        let numbers = find_numbers_in_row(data[y]);
+
+        for (number, start_pos, end_pos) in numbers {
+            for rx in start_pos..=end_pos {
+                numbers_map.insert((y, rx), number);
+            }
+        }
+    }
+
+    let gears = find_gears_positions(data);
+    for (gear_y, gear_x) in gears {
+        let Some(ids) = check_adjacent_for_numbers(gear_y, gear_x, &numbers_map) else {
+            continue;
+        };
+
+        if ids.len().eq(&2) {
+            // Calculate gear ratio
+            current_sum += ids[0] * ids[1];
+        }
+    }
+
+    current_sum
+}
+
 #[cfg(test)]
 mod tests {
     use rstest::*;
@@ -91,8 +171,17 @@ mod tests {
     #[rstest]
     #[case(TEST_DATA_0, 4361)]
     #[case(TEST_DATA_1, 521515)]
-    fn dummy_test<const N: usize>(#[case] data: [&[u8]; N], #[case] expected: u32) {
+    fn find_sum_of_adjacent_numbers_test<const N: usize>(#[case] data: [&[u8]; N], #[case] expected: u32) {
         let actual = find_sum_of_adjacent_numbers(&data);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[rstest]
+    #[case(TEST_DATA_0, 467835)]
+    #[case(TEST_DATA_1, 69527306)]
+    fn find_sum_of_adjacent_numbers_for_gears_test<const N: usize>(#[case] data: [&[u8]; N], #[case] expected: u32) {
+        let actual = find_sum_of_adjacent_numbers_for_gears(&data);
 
         assert_eq!(expected, actual);
     }
