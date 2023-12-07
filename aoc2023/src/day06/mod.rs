8cec647d922602c06a065d18b953651d288e99df
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2023-12-06 20:09:45 -0500

    Add part 1 day 6

--- aoc2023/src/day06/mod.rs
+++ aoc2023/src/day06/mod.rs
@@ -1,12 +1,41 @@
 #[cfg(test)] mod data;
 
+fn find_product_of_win_counts(data: &[(u32, u32)]) -> u32 {
+    let mut current_product = 1;
+
+    for (max_time, record_distance) in data {
+        let mut win_count = 0;
+
+        for wait_time in 1..*max_time {
+            let rem_time = max_time - wait_time;
+            let distance = wait_time * rem_time;
+
+            //println!("Distance: {distance}");
+
+            if distance > *record_distance {
+                win_count += 1;
+            }
+        }
+
+        current_product *= win_count;
+
+        //println!("Wins: {win_count}\n\n");
+    }
+
+    current_product
+}
+
 #[cfg(test)]
 mod tests {
     use rstest::*;
     use super::{*, data::*};
 
     #[rstest]
-    fn dummy_test() {
-        assert_eq!(1, 1);
+    #[case(TEST_DATA_0, 288)]
+    #[case(TEST_DATA_1, 1731600)]
+    fn dummy_test<const N: usize>(#[case] data: [(u32, u32); N], #[case] expected: u32) {
+        let actual = find_product_of_win_counts(&data);
+
+        assert_eq!(expected, actual);
     }
 }
