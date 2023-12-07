89e076803168ea432556e29753a96f5b86cece5d
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2023-12-07 08:19:35 -0500

    Add day 7 test data

--- aoc2023/src/day07/mod.rs
+++ aoc2023/src/day07/mod.rs
@@ -1,12 +1,19 @@
 #[cfg(test)] mod data;
 
+fn calculate_hand_winnings(hands: &[(&[u8], u32)]) -> u32 {
+    0
+}
+
 #[cfg(test)]
 mod tests {
     use rstest::*;
     use super::{*, data::*};
 
     #[rstest]
-    fn dummy_test() {
-        assert_eq!(1, 1);
+    #[case(TEST_DATA_0, 6440)]
+    fn calculate_hand_winnings_test<const N: usize>(#[case] data: [(&[u8], u32); N], #[case] expected: u32) {
+        let actual = calculate_hand_winnings(&data);
+
+        assert_eq!(expected, actual);
     }
 }
