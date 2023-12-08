b31dd271113e2047633b8ac009ed8a7385ec1b77
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2023-12-08 00:40:36 -0500

    Add part 1 day 8

--- aoc2023/src/day08/mod.rs
+++ aoc2023/src/day08/mod.rs
@@ -1,4 +1,57 @@
+use std::collections::HashMap;
+
 #[cfg(test)] mod data;
+
+fn find_steps_to_end(data_nav: &[u8], data_nodes: &[(&[u8; 3], &[u8; 3], &[u8; 3])]) -> u32 {
+    let map_index = data_nodes
+        .iter()
+        .enumerate()
+        .map(|(i, (n, ..))| (*n, i))
+        .collect::<HashMap<_, _>>();
+
+    let start = map_index.get(b"AAA").map(|s| *s).unwrap();
+    let end = map_index.get(b"ZZZ").map(|s| *s).unwrap();
+
+    let mut current_step = 0;
+    let mut current_pos = 0;
+    let mut current_node = &data_nodes[start];
+
+    loop {
+        let is_end = match current_node {
+            (b"ZZZ", ..) => true,
+            _ => false,
+        };
+
+        if is_end {
+            break;
+        }
+
+        let check_left = data_nav[current_pos].eq(&b'L');
+
+        // Update node
+        current_node = if check_left {
+            let (_, left_node, _) = current_node;
+            let left_idx = map_index.get(left_node).map(|s| *s).unwrap();
+
+            &data_nodes[left_idx]
+        } else {
+            let (_, _, right_node) = current_node;
+            let right_idx = map_index.get(right_node).map(|s| *s).unwrap();
+
+            &data_nodes[right_idx]
+        };
+
+        // Update steps
+        current_step += 1;
+        current_pos = if (data_nav.len() - 1) == current_pos {
+            0
+        } else {
+            current_pos + 1
+        };
+    }
+
+    current_step
+}
 
 #[cfg(test)]
 mod tests {
@@ -6,7 +59,12 @@ mod tests {
     use super::{*, data::*};
 
     #[rstest]
-    fn dummy_test() {
-        assert_eq!(1, 1);
+    #[case(TEST_DATA_0_NAV, TEST_DATA_0_NODES, 2)]
+    #[case(TEST_DATA_1_NAV, TEST_DATA_1_NODES, 6)]
+    #[case(TEST_DATA_2_NAV, TEST_DATA_2_NODES, 13207)]
+    fn find_steps_to_end_test<const N: usize>(#[case] data_nav: &[u8], #[case] data_nodes: [(&[u8; 3], &[u8; 3], &[u8; 3]); N], #[case] expected: u32) {
+        let actual = find_steps_to_end(data_nav, &data_nodes);
+
+        assert_eq!(expected, actual);
     }
 }
