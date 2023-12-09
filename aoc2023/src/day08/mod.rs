f7c095210f26161c8bc0f218b297ec19e38e57fc
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2023-12-08 22:00:24 -0500

    Add part 2 day 8

--- aoc2023/src/day08/mod.rs
+++ aoc2023/src/day08/mod.rs
@@ -52,14 +52,14 @@ fn find_steps_to_end(data_nav: &[u8], data_nodes: &[(&[u8; 3], &[u8; 3], &[u8; 3
     current_step
 }
 
-fn find_steps_to_end_2(data_nav: &[u8], data_nodes: &[(&[u8; 3], &[u8; 3], &[u8; 3])]) -> u32 {
+fn find_steps_to_end_2(data_nav: &[u8], data_nodes: &[(&[u8; 3], &[u8; 3], &[u8; 3])]) -> u64 {
     let map_index = data_nodes
         .iter()
         .enumerate()
         .map(|(i, (n, ..))| (*n, i))
         .collect::<HashMap<_, _>>();
 
-    let (all_starts, all_ends) = data_nodes
+    /*let (all_starts, all_ends) = data_nodes
         .iter()
         .enumerate()
         .fold((HashSet::new(), HashSet::new()), |(mut starts, mut ends), (i, (n, ..))| {
@@ -74,23 +74,30 @@ fn find_steps_to_end_2(data_nav: &[u8], data_nodes: &[(&[u8; 3], &[u8; 3], &[u8;
             }
 
             (starts, ends)
-        });
+        });*/
 
     let mut current_step = 0;
     let mut current_pos = 0;
     let mut current_nodes = data_nodes
         .iter()
         .enumerate()
-        .filter(|(i, _)| all_starts.contains(&i))
+        //.filter(|(i, _)| all_starts.contains(&i))
+        .filter(|(i, ([_, _, c], ..))| c.eq(&b'A'))
         .map(|(_, d)| d)
         .collect::<Vec<_>>();
 
-    loop {
-        let is_end = current_nodes
-            .iter()
-            .all(|([_, _, c], ..)| c.eq(&b'Z'));
+    let mut visited_nodes = current_nodes
+        .iter()
+        .map(|_| HashMap::new())
+        .collect::<Vec<_>>();
 
-        if is_end {
+    loop {
+        let all_visited = current_nodes
+            .iter()
+            .enumerate()
+            .all(|(i, (n, ..))| visited_nodes[i].contains_key(&(*n, current_pos)));
+
+        if all_visited {
             break;
         }
 
@@ -98,16 +105,28 @@ fn find_steps_to_end_2(data_nav: &[u8], data_nodes: &[(&[u8; 3], &[u8; 3], &[u8;
 
         // Update nodes
         if check_left {
-            for current_node in current_nodes.iter_mut() {
-                let (_, left_node, _) = current_node;
+            for (current_node, visited) in current_nodes.iter_mut().zip(visited_nodes.iter_mut()) {
+                let (n, left_node, _) = current_node;
                 let left_idx = map_index.get(left_node).map(|s| *s).unwrap();
+
+                if visited.contains_key(&(*n, current_pos)) {
+                    //println!("Cycle detected (Pos: {current_pos}, Step: {current_step})");
+                } else {
+                    visited.insert((*n, current_pos), current_step);
+                }
 
                 *current_node = &data_nodes[left_idx];
             }
         } else {
-            for current_node in current_nodes.iter_mut() {
-                let (_, _, right_node) = current_node;
+            for (current_node, visited) in current_nodes.iter_mut().zip(visited_nodes.iter_mut()) {
+                let (n, _, right_node) = current_node;
                 let right_idx = map_index.get(right_node).map(|s| *s).unwrap();
+
+                if visited.contains_key(&(*n, current_pos)) {
+                    //println!("Cycle detected (Pos: {current_pos}, Step: {current_step})");
+                } else {
+                    visited.insert((*n, current_pos), current_step);
+                }
 
                 *current_node = &data_nodes[right_idx];
             }
@@ -122,7 +141,61 @@ fn find_steps_to_end_2(data_nav: &[u8], data_nodes: &[(&[u8; 3], &[u8; 3], &[u8;
         };
     }
 
-    current_step
+    // Remove all non-ending nodes
+    visited_nodes
+        .iter_mut()
+        .for_each(|nodes| {
+            let non_z_keys = nodes
+                .iter()
+                .filter(|(([_, _, c], ..), _)| c.ne(&b'Z'))
+                .map(|(k, v)| *k)
+                .collect::<Vec<_>>();
+
+            for key in non_z_keys {
+                nodes.remove(&key);
+            }
+        });
+
+    // Get lowest distinct prime numbers and multiply together
+    visited_nodes
+        .into_iter()
+        .map(|map| {
+            let min_number = map.into_values().min().unwrap() as u64;
+            get_prime_factors(min_number)
+        })
+        .flatten()
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .product::<u64>()
+}
+
+
+fn is_prime(n: u64) -> bool {
+    if n <= 1 {
+        return false;
+    }
+
+    (2..n)
+        .into_iter()
+        .find_map(|a| if n % a == 0 {
+            Some(false)
+        } else {
+            None
+        })
+        .unwrap_or(true)
+}
+
+// Definitely not the most efficient
+fn get_prime_factors(n: u64) -> Vec<u64> {
+    if n == 1 {
+        return Vec::new();
+    }
+
+    (2..=n)
+        .into_iter()
+        //.filter(|&x| n % x == 0)
+        .filter(|a| n % a == 0 && is_prime(*a))
+        .collect::<Vec<_>>()
 }
 
 #[cfg(test)]
@@ -142,8 +215,8 @@ mod tests {
 
     #[rstest]
     #[case(TEST_DATA_3_NAV, TEST_DATA_3_NODES, 6)]
-    #[case(TEST_DATA_2_NAV, TEST_DATA_2_NODES, 0)]
-    fn find_steps_to_end_2_test<const N: usize>(#[case] data_nav: &[u8], #[case] data_nodes: [(&[u8; 3], &[u8; 3], &[u8; 3]); N], #[case] expected: u32) {
+    #[case(TEST_DATA_2_NAV, TEST_DATA_2_NODES, 12_324_145_107_121)]
+    fn find_steps_to_end_2_test<const N: usize>(#[case] data_nav: &[u8], #[case] data_nodes: [(&[u8; 3], &[u8; 3], &[u8; 3]); N], #[case] expected: u64) {
         let actual = find_steps_to_end_2(data_nav, &data_nodes);
 
         assert_eq!(expected, actual);
