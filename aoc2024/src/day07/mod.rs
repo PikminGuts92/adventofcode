78df1a7ae8653fa90bf454843c77b5946fb7a650
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2024-12-18 22:50:14 -0500

    Add day 7 part 2

--- aoc2024/src/day07/mod.rs
+++ aoc2024/src/day07/mod.rs
@@ -1,11 +1,7 @@
 #[cfg(test)] mod data;
 
-/*enum Operation {
-    Add,
-    Multiply,
-}*/
-
-fn calculate_equation_solve_recursive(result: u64, current: u64, numbers: &[u64]) -> u64 {
+// Part 1
+fn calculate_add_multiply_equation_solve_recursive(result: u64, current: u64, numbers: &[u64]) -> u64 {
     if current > result {
         return 0;
     } else if numbers.is_empty() {
@@ -16,23 +12,61 @@ fn calculate_equation_solve_recursive(result: u64, current: u64, numbers: &[u64]
 
     // Calculate add
     let add_result = current + numbers[0];
-    solve_count += calculate_equation_solve_recursive(result, add_result, &numbers[1..]);
+    solve_count += calculate_add_multiply_equation_solve_recursive(result, add_result, &numbers[1..]);
 
     // Calculate multiply
     let multiply_result = current * numbers[0];
-    solve_count += calculate_equation_solve_recursive(result, multiply_result, &numbers[1..]);
+    solve_count += calculate_add_multiply_equation_solve_recursive(result, multiply_result, &numbers[1..]);
 
     solve_count
 }
 
-fn calculate_equation_solve_count(result: u64, numbers: &[u64]) -> u64 {
-    calculate_equation_solve_recursive(result, numbers[0], &numbers[1..])
+fn calculate_add_multiply_equation_solve_count(result: u64, numbers: &[u64]) -> u64 {
+    calculate_add_multiply_equation_solve_recursive(result, numbers[0], &numbers[1..])
 }
 
-fn calculate_sum_of_solvable_equations(data: &[(u64, &[u64])]) -> u64 {
+fn calculate_sum_of_solvable_add_multiply_equations(data: &[(u64, &[u64])]) -> u64 {
     data
         .iter()
-        .filter(|(res, nums)| calculate_equation_solve_count(*res, nums) > 0)
+        .filter(|(res, nums)| calculate_add_multiply_equation_solve_count(*res, nums) > 0)
+        .map(|(res, _)| *res)
+        .sum()
+}
+
+// Part 2
+fn calculate_add_multiply_concat_equation_solve_recursive(result: u64, current: u64, numbers: &[u64]) -> u64 {
+    if current > result {
+        return 0;
+    } else if numbers.is_empty() {
+        return if current == result { 1 } else { 0 };
+    }
+
+    let mut solve_count = 0;
+
+    // Calculate add
+    let add_result = current + numbers[0];
+    solve_count += calculate_add_multiply_concat_equation_solve_recursive(result, add_result, &numbers[1..]);
+
+    // Calculate multiply
+    let multiply_result = current * numbers[0];
+    solve_count += calculate_add_multiply_concat_equation_solve_recursive(result, multiply_result, &numbers[1..]);
+
+    // Calculate concat (hacky way w/ strings)
+    //let mut digits = Vec::new();
+    let concat_result = format!("{}{}", current, numbers[0]).parse::<u64>().unwrap();
+    solve_count += calculate_add_multiply_concat_equation_solve_recursive(result, concat_result, &numbers[1..]);
+
+    solve_count
+}
+
+fn calculate_add_multiply_concat_equation_solve_count(result: u64, numbers: &[u64]) -> u64 {
+    calculate_add_multiply_concat_equation_solve_recursive(result, numbers[0], &numbers[1..])
+}
+
+fn calculate_sum_of_solvable_add_multiply_concat_equations(data: &[(u64, &[u64])]) -> u64 {
+    data
+        .iter()
+        .filter(|(res, nums)| calculate_add_multiply_concat_equation_solve_count(*res, nums) > 0)
         .map(|(res, _)| *res)
         .sum()
 }
@@ -45,8 +79,17 @@ mod tests {
     #[rstest]
     #[case(TEST_DATA_0, 3749)]
     #[case(TEST_DATA_1, 303766880536)]
-    fn calculate_sum_of_solvable_equations_test<const N: usize>(#[case] data: [(u64, &[u64]); N], #[case] expected: u64) {
-        let actual = calculate_sum_of_solvable_equations(&data);
+    fn calculate_sum_of_solvable_add_multiply_equations_test<const N: usize>(#[case] data: [(u64, &[u64]); N], #[case] expected: u64) {
+        let actual = calculate_sum_of_solvable_add_multiply_equations(&data);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[rstest]
+    #[case(TEST_DATA_0, 11387)]
+    #[case(TEST_DATA_1, 337041851384440)]
+    fn calculate_sum_of_solvable_add_multiply_concat_equations_test<const N: usize>(#[case] data: [(u64, &[u64]); N], #[case] expected: u64) {
+        let actual = calculate_sum_of_solvable_add_multiply_concat_equations(&data);
 
         assert_eq!(expected, actual);
     }
